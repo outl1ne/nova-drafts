4d6f8eb6c1be1ca0a509c551e031000ee6a60653
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-06 10:04:26 +0200

    add DraftButton __construct method

--- src/DraftButton.php
+++ src/DraftButton.php
@@ -12,4 +12,23 @@ class DraftButton extends Field
      * @var string
      */
     public $component = 'nova-draft-button';
+
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        $this->withMeta([
+            'asHtml' => true,
+        ]);
+
+        $this->hideFromIndex();
+    }
 }
