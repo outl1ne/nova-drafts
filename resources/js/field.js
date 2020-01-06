dc0be814810cc00dafb62e63697ae162725ee0a7
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-06 15:39:51 +0200

    update webpack js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,4 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-nova-draft-button', require('./components/IndexField'))
-  Vue.component('detail-nova-draft-button', require('./components/DetailField'))
-  Vue.component('form-nova-draft-button', require('./components/FormField'))
+  Vue.component('detail-nova-draft-button', require('./nova-draft-button/components/DetailButton'))
+  Vue.component('form-nova-draft-button', require('./nova-draft-button/components/FormButton'))
 })
