75cffa3b296990bdb67ef5363ba5ef8c40cc0157
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-07 15:21:49 +0200

    add draft controller

--- src/Http/DraftController.php
+++ src/Http/DraftController.php
@@ -4,10 +4,38 @@ namespace OptimistDigital\NovaDrafts\Http;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+use OptimistDigital\NovaDrafts\Models\Draft;
 
 class DraftController extends Controller
 {
-    public function publishDraft($draftId, Request $request) {
-        return true;
+    public function publishDraft($draftId, Request $request)
+    {
+        $resource_class = $request->input('class');
+        $draft_to_publish = $resource_class::find($draftId);
+
+        if (isset($draft_to_publish)) {
+            $draft_parent = Draft::draftParent($resource_class, $draft_to_publish->draft_parent_id);
+
+            if ($draft_parent !== null) {
+                $published_draft = $draft_parent;
+                foreach ($draft_to_publish->toArray() as $draft_column => $value) {
+                    if (!in_array($draft_column, ['id', 'draft_parent_id', 'preview_token', 'created_at'])) {
+                        $published_draft[$draft_column] = $draft_to_publish[$draft_column];
+                    }
+                }
+                $published_draft->published = true;
+                $published_draft->save();
+                $draft_to_publish->delete();
+                Log::warning(print_r($published_draft, true));
+                return $published_draft;
+            } else {
+                $draft_to_publish->published = true;
+                $draft_to_publish->preview_token = null;
+                $draft_to_publish->save();
+                return $draft_to_publish;
+            }
+        }
+        return $draft_to_publish;
     }
 }
