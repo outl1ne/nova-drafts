a0855fe2b6c42242901ed0811632d02535c99dbd
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-07 13:18:05 +0200

    add published field

---
+++ src/PublishedField.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OptimistDigital\NovaBlog\Nova\Fields;
+
+use Laravel\Nova\Fields\Field;
+use OptimistDigital\NovaDrafts\Models\Draft;
+
+class PublishedField extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-published-field';
+
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, 'published', $resolveCallback);
+
+        $this->withMeta([
+            'asHtml' => true,
+        ]);
+
+        $this->exceptOnForms();
+    }
+
+    public function resolve($resource, $attribute = null)
+    {
+        parent::resolve($resource, $attribute);
+
+        $this->withMeta([
+            'childDraft' => Draft::childDraft(get_class($resource), $resource->id),
+            'draftParent' => Draft::draftParent(get_class($resource), $resource->draft_parent_id)
+        ]);
+    }
+}
