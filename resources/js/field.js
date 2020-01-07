357d98e5f75241d512092e37bc92513231f71810
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-07 12:21:04 +0200

    add published field components

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,7 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('detail-nova-draft-button', require('./nova-draft-button/components/DetailButton'))
-  Vue.component('form-nova-draft-button', require('./nova-draft-button/components/FormButton'))
-})
+    Vue.component('detail-nova-draft-button', require('./nova-draft-button/components/DetailButton'));
+    Vue.component('form-nova-draft-button', require('./nova-draft-button/components/FormButton'));
+
+    Vue.component('index-nova-published-field', require('./nova-published-field/components/IndexField'));
+    Vue.component('detail-nova-published-field', require('./nova-published-field/components/DetailButton'));
+});
