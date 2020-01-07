291560f0fc709fa78c828c37b36095d353c01209
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-07 11:09:17 +0200

    save original model when draft

--- src/DraftButton.php
+++ src/DraftButton.php
@@ -38,37 +38,21 @@ class DraftButton extends Field
     {
         parent::resolve($resource, $attribute);
 
-        $draft_model = new Draft();
         $this->withMeta([
-            'childDraft' => $draft_model->childDraft(get_class($resource)),
+            'childDraft' => $resource->childDraft,
             'isDraft' => (isset($resource->draft_parent_id) || (!isset($resource->draft_parent_id) && !$resource->published && isset($resource->id))),
         ]);
     }
 
     protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
     {
-       $draft_model = new Draft();
+        $draft_model = new Draft();
         if (isset($request->draft) && $draft_model->draftsEnabled()) {
             Draft::createDraft($model);
-            //$model->fill(collect($model->getOriginal())->except()->toArray());
-            //$this->revertToOriginal($model);
-            dd($model);
+            $model->refresh();
+            $model->save();
             return true;
         }
-    }
-
-    protected function revertToOriginal($model)
-    {
-        $fillableProperties = $model->fillable;
-
-        $originalFillables = array_filter(
-            $model->getOriginal()->except('id'),
-            function ($key) use ($fillableProperties ) {
-                return in_array($key, $fillableProperties);
-            },
-            ARRAY_FILTER_USE_KEY
-        );
-
-        $model->fill($originalFillables);
+        return true;
     }
 }
