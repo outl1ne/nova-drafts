90d380fd1d80823f933dd2bca2de2c8e5cde879e
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-07 14:02:22 +0200

    add routes when booted

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace OptimistDigital\NovaDrafts;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
 use OptimistDigital\NovaDrafts\Commands\CreateDraftsMigration;
+use OptimistDigital\NovaDrafts\Http\Middleware\Authorize;
 
 class FieldServiceProvider extends ServiceProvider
 {
@@ -20,12 +22,26 @@ class FieldServiceProvider extends ServiceProvider
             Nova::script('nova-drafts', __DIR__ . '/../dist/js/field.js');
         });
 
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
         // Register commands
         if ($this->app->runningInConsole()) {
             $this->commands([
                 CreateDraftsMigration::class,
             ]);
         }
+    }
+
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+        Route::middleware(['nova', Authorize::class])
+            ->prefix('nova-vendor/nova-drafts')
+            ->group(__DIR__ . '/../routes/api.php');
     }
 
     /**
