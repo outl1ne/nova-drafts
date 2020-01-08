319a3102dd290668ae5292275fc23d7370edcd54
KasparRosin <kaspar.rosin@tptlive.ee>
2020-01-08 11:32:36 +0200

    should show draft button logic

--- src/DraftButton.php
+++ src/DraftButton.php
@@ -4,6 +4,7 @@ namespace OptimistDigital\NovaDrafts;
 
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Http\Requests\ResourceDetailRequest;
 use OptimistDigital\NovaDrafts\Models\Draft;
 
 class DraftButton extends Field
@@ -14,6 +15,8 @@ class DraftButton extends Field
      * @var string
      */
     public $component = 'nova-draft-button';
+
+    protected $enabled = true;
 
     /**
      * Create a new field.
@@ -26,22 +29,44 @@ class DraftButton extends Field
     public function __construct($name, $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback);
-
         $this->withMeta([
             'asHtml' => true,
         ]);
 
+        if (!$this->enabled) {
+            $this->hideWhenUpdating();
+            $this->hideFromDetail();
+            $this->hideWhenCreating();
+        }
         $this->hideFromIndex();
     }
 
     public function resolve($resource, $attribute = null)
     {
-        parent::resolve($resource, $attribute);
+        if ($this->showDraftButton($resource)) {
+            parent::resolve($resource, $attribute);
 
-        $this->withMeta([
-            'childDraft' => Draft::childDraft(get_class($resource), $resource->id),
-            'isDraft' => (isset($resource->draft_parent_id) || (!isset($resource->draft_parent_id) && !$resource->published && isset($resource->id))),
-        ]);
+            $this->withMeta([
+                'childDraft' => Draft::childDraft(get_class($resource), $resource->id),
+                'isDraft' => (isset($resource->draft_parent_id) || (!isset($resource->draft_parent_id) && !$resource->published && isset($resource->id))),
+            ]);
+        }
+    }
+
+    protected function showDraftButton($resource)
+    {
+        return !(!$this->isDraft($resource) && (request() instanceof ResourceDetailRequest)) || isset($this->childDraft);
+    }
+
+    protected function isDraft($resource)
+    {
+        return (isset($resource->draft_parent_id) || (!isset($resource->draft_parent_id) && !$resource->published && isset($resource->id)));
+    }
+
+    public function draftsEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+        return $this;
     }
 
     protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
