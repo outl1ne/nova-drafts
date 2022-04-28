4b92db6b04dbd960755199fb126c3a3a293f4c80
Chris Vasey <chrismvasey@gmail.com>
2022-04-28 16:38:30 +0100

    Updated dependencies for Nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,16 @@
 let mix = require('laravel-mix');
+let path = require('path')
 
-mix.setPublicPath('dist').js('resources/js/field.js', 'js').vue();
+mix.js('resources/js/field.js', 'js').vue({ version: 3 })
+  .webpackConfig({
+    externals: {
+      vue: 'Vue',
+    },
+    output: {
+      uniqueName: 'optimistdigital/nova-drafts',
+    }
+})
+
+mix.alias({
+  'laravel-nova': path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js'),
+})
